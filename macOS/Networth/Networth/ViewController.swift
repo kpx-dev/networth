cd0e5bb914f1b444cd65d30be2d1da88afde1a41
Kien Pham <kien@kienpham.com>
2018-08-16 13:38:54 -0700

    adding macOS version

---
+++ macOS/Networth/Networth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Networth
+//
+//  Created by Kien Pham on 8/16/18.
+//  Copyright © 2018 Networth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
