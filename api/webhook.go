9854bf22c0b9eb3912e6eefc3a1687393ab7b21d
Kien Pham <kien@kienpham.com>
2018-08-30 01:44:41 -0700

    fix

--- api/webhook.go
+++ api/webhook.go
@@ -38,8 +38,8 @@ func (s *NetworthAPI) handleWebhook() http.HandlerFunc {
 			return
 		}
 
-		fmt.Println("webhook body is")
-
+		ips := r.Header.Get("X-Forwarded-For")
+		fmt.Println("Got webhook message from these ips: ", ips, r.RemoteAddr)
 		alert("New webhook, type:" + body.WebhookType + " code: " + body.WebhookCode)
 		successResp(w, body)
 	}
