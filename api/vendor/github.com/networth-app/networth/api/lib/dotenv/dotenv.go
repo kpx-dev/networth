0e9d3b39fe1497ba531026087c916a8c1f939cee
Kien Pham <kien@kienpham.com>
2018-09-04 21:55:52 -0700

    z

---
+++ api/vendor/github.com/networth-app/networth/api/lib/dotenv/dotenv.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	dir, _ := os.Getwd()
+	fmt.Println("the .env root dir is", dir)
+
+	if strings.HasSuffix(dir, "/api") {
+		dir = strings.Replace(dir, "/api", "", 1)
+	}
+
+	envPath := fmt.Sprintf("%s/.env", dir)
+	file, _ := os.Open(envPath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplitted := strings.Split(line, "=")
+
+		key := strings.TrimSpace(lineSplitted[0])
+		val := strings.TrimSpace(lineSplitted[1])
+		fmt.Println("Settin env ", key, val)
+		os.Setenv(key, val)
+	}
+}
