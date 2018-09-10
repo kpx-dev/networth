f3ac825924e51f9b68626900dcdfabdb4038ad6d
Kien Pham <kien@kienpham.com>
2018-09-09 18:04:09 -0700

    adding notification

---
+++ api/notification/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/networth-app/networth/api/lib"
+)
+
+var (
+	slackWebhookURL = nwlib.GetEnv("SLACK_WEBHOOK_URL")
+	slackChannel    = nwlib.GetEnv("SLACK_CHANNEL")
+)
+
+func handleSNSNotification(ctx context.Context, snsEvent events.SNSEvent) {
+	messages := ""
+	for _, record := range snsEvent.Records {
+		messages += record.SNS.Message + "\n"
+	}
+
+	nwlib.PublishSlack(slackWebhookURL, messages, slackChannel)
+
+}
+
+func main() {
+	lambda.Start(handleSNSNotification)
+}
