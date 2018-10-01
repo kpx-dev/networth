c9dc9577cca88e5b0038153f8b8a0b353686447e
Kien Pham <kien@kienpham.com>
2018-09-30 23:18:55 -0700

    z

--- cloud/networth.tf
+++ cloud/networth.tf
@@ -316,44 +316,39 @@ resource "aws_s3_bucket" "LoggingS3Bucket" {
   bucket = "log.${var.DomainName}"
 }
 
+//=====================================================
+// API Gateway
+//=====================================================
 resource "aws_api_gateway_rest_api" "api" {
   name = "${var.AppName}"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+// creates /api
+resource "aws_api_gateway_resource" "api" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "api"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
+// create /api/networth
+resource "aws_api_gateway_resource" "networth" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.api.invoke_arn}"
+  parent_id   = "${aws_api_gateway_resource.api.id}"
+  path_part   = "networth"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
+resource "aws_api_gateway_method" "get_networth" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.networth.id}"
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = "${aws_api_gateway_authorizer.auth.id}"
+}
+
+resource "aws_api_gateway_integration" "get_networth" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  resource_id = "${aws_api_gateway_method.get_networth.resource_id}"
+  http_method = "${aws_api_gateway_method.get_networth.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -361,13 +356,13 @@ resource "aws_api_gateway_integration" "lambda_root" {
 }
 
 resource "aws_api_gateway_deployment" "api" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "latest"
+  # TODO: https://github.com/hashicorp/terraform/issues/6613
+  # stage_description = "${md5(file("networth.tf"))}"
+  depends_on = [
+    "aws_api_gateway_integration.get_networth",
+  ]
 }
 
 resource "aws_api_gateway_authorizer" "auth" {
