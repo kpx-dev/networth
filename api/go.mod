e8791cc685be85e1f1ba2a18316eb62fcdc6105a
Kien Pham <kien@kienpham.com>
2018-10-08 01:05:33 -0700

    adding chart

--- api/go.mod
+++ api/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/go-redis/redis v6.14.1+incompatible
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
-	github.com/networth-app/networth/api/lib v0.0.0-20181007235747-9f9de2556363
+	github.com/networth-app/networth/api/lib v0.0.0-20181008000548-a6831a92ad1e
 	github.com/onsi/gomega v1.4.2 // indirect
 	github.com/pkg/errors v0.8.0 // indirect
 	github.com/rs/cors v1.5.0
--- web/src/views/DashboardTab.jsx
+++ web/src/views/DashboardTab.jsx
@@ -15,10 +15,40 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 
+import {Line} from 'react-chartjs-2';
+
 class DashboardTab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {networth: 0};
+
+    this.chartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Net Worth',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
+
   }
 
   static propTypes = {
@@ -88,6 +118,13 @@ class DashboardTab extends React.Component {
           </GridItem>
         </GridContainer>
 
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+
+          <Line data={this.chartData} width="600" height="250" />
+
+          </GridItem>
+        </GridContainer>
         </div>
       </div>
     );
