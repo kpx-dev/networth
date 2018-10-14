3a5d2d908bba1bfac94657784e1be5ddb13760a9
Kien Pham <kien@kienpham.com>
2018-10-14 00:43:51 -0700

    z

--- web/src/views/DashboardTab.jsx
+++ web/src/views/DashboardTab.jsx
@@ -21,33 +21,6 @@ class DashboardTab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {networth: 0};
-
-    // this.chartData = {
-    //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    //   datasets: [
-    //     {
-    //       label: 'Net Worth',
-    //       backgroundColor: 'rgba(75,192,192,0.4)',
-    //       borderColor: 'rgba(75,192,192,1)',
-    //       pointBorderColor: 'rgba(75,192,192,1)',
-    //       pointBackgroundColor: '#fff',
-    //       pointBorderWidth: 5,
-    //       pointHoverRadius: 10,
-    //       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-    //       pointHoverBorderColor: 'rgba(220,220,220,1)',
-    //       pointHoverBorderWidth: 2,
-    //       pointRadius: 1,
-    //       pointHitRadius: 10,
-    //       // fill: false,
-    //       // lineTension: 0.1,
-    //       // borderCapStyle: 'butt',
-    //       // borderDash: [],
-    //       // borderDashOffset: 0.0,
-    //       // borderJoinStyle: 'miter',
-    //       data: [65, 59, 80, 81, 56, 55, 40]
-    //     }
-    //   ]
-    // };
   }
 
   static propTypes = {
@@ -92,7 +65,7 @@ class DashboardTab extends React.Component {
       data: [] };
     const liabilitiesSet = {
       label: 'Liabilities',
-      // fill: false,
+      fill: false,
       backgroundColor: 'rgba(75,192,192,0.4)',
       borderColor: 'red',
       pointBorderColor: 'red',
